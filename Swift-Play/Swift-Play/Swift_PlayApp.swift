ade91c8b7b7affd2b08068094c4d0c9ae91e78d4
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-05-12 22:50:21 +0530

    Initial Commit
    initial commit

---
+++ Swift-Play/Swift-Play/Swift_PlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_PlayApp.swift
+//  Swift-Play
+//
+//  Created by Partha on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_PlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift-Play/Swift-PlayTests/Swift_PlayTests.swift
@@ -0,0 +1,16 @@
+//
+//  Swift_PlayTests.swift
+//  Swift-PlayTests
+//
+//  Created by Partha on 12/05/25.
+//
+
+import Testing
+
+struct Swift_PlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
